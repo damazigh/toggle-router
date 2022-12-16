8d79290d14c6c1b650752719b62a023c7edd5f1c
Patrick Irlande <101345743+pirlande-fw@users.noreply.github.com>
2022-12-16 13:56:37 +0100

    Add metadata endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EnvController } from './env.controller';
 import { EnvService } from './env.service';
+import { MetadataController } from './metadata.controller';
 
 @Module({
   imports: [],
-  controllers: [EnvController],
+  controllers: [EnvController, MetadataController],
   providers: [AppService, EnvService],
 })
 export class AppModule {}
