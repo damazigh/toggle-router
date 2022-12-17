be94c8b038cc0169e19734da9cb9bc5f95178272
motomasini <morgantomasini@gmail.com>
2022-12-17 21:58:52 +0100

    enable corse / reformat metadata

--- src/metadata.controller.ts
+++ src/metadata.controller.ts
@@ -1,16 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
-import { SupportedToggleType, SupportedAppliesTo, SupportedEnvType } from './enum/constant';
+import { SupportedEntities, SupportedAppliesToForBasic } from './enum/constant';
 
 @Controller('metadata')
 export class MetadataController {
-
-    @Get()
-    public all() {
-        return {
-            "env_types": SupportedEnvType,
-            "toggle_types": SupportedToggleType,
-            "scopes": SupportedAppliesTo
-        }
-    }
+  @Get()
+  public all() {
+    return [
+      {
+        name: 'Region',
+        type: 'basic',
+        options: Object.keys(SupportedAppliesToForBasic).map((v) => ({
+          name: SupportedAppliesToForBasic[v],
+          key: v,
+        })),
+      },
+      {
+        name: 'Granular',
+        type: 'toggle',
+        options: Object.keys(SupportedEntities).map((v) => ({
+          name: SupportedEntities[v],
+          key: v,
+        })),
+      },
+    ];
+  }
 }
-
