809589a0f5ef0753a3eef470ce1360357b12d4d6
Patrick Irlande <101345743+pirlande-fw@users.noreply.github.com>
2022-12-17 21:34:25 +0100

    Add GET endpoint for env entities

--- src/env-entity.service.ts
+++ src/env-entity.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { EnvEntityCommand } from './db/command/impl/env-entity.command';
 import { CreateEnvEntity } from './inout/in/create-env-entity.model';
+import { GetEnvEntities } from './inout/in/get_env_entities';
+import { EnvEntityOutput } from './inout/out/env_entity_output';
 
 @Injectable()
 export class EnvEntityService {
@@ -11,4 +13,13 @@ export class EnvEntityService {
     const command = commandWrapper.buildBatchWriteCommandInput();
     return await commandWrapper.createBatch(command)
   }
+
+  async getEnvEntities(getEnvEntities: GetEnvEntities) {
+    const commandWrapper = new EnvEntityCommand(getEnvEntities);
+    commandWrapper.validateForQuery();
+    const queryCommands = commandWrapper.buildQueryCommands();
+    let items = (await commandWrapper.query(queryCommands)).flat();
+    return items.map((item) => new EnvEntityOutput(item));
+  }
+
 }
---
+++ src/inout/in/get_env_entities.ts
@@ -0,0 +1,3 @@
+export class GetEnvEntities {
+    toggleKey: string;
+}
