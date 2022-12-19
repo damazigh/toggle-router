10dee71dd9ae6a9fd130bff0286cf1c2adfeaff6
damazigh <adjebarri@fieldwire.com>
2022-12-19 14:20:25 +0100

    add data for toggles

--- src/inout/out/toggle_output.ts
+++ src/inout/out/toggle_output.ts
@@ -2,10 +2,12 @@ export class ToggleOutput {
     toggleSortKey: string
     toggleType: string
     appliesTo: string
+    data: any
 
     constructor(item: any) {
         this.toggleSortKey = item.SK;
         this.toggleType = item.toggleType;
         this.appliesTo = item.appliesTo;
+        this.data = item.data
     }
 }
